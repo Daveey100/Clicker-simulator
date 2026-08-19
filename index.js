2ce3a0dd2d64415db4386cfbca5ece101f54507d
David <ddave0641@gmail.com>
2026-08-18 21:58:46 -0300

    dot comma

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ let shotN = JSON.parse(localStorage.getItem('shots')) ?? 0;
 
     shotCounter.innerHTML = `<p>You shot: ${shotN}</p>`;
 
-  })
+  });
 
   resetButton.addEventListener('click', () => {
     shotN = 0;
     localStorage.setItem('shots', JSON.stringify(shotN));
 
     shotCounter.innerHTML = `<p>You shot: ${shotN}</p>`;
-  })
+  });
 
+  
 
 
 
