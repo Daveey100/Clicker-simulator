4acbaeb095f8c9207c6a8d9dd49cb3140bab5fec
David <ddave0641@gmail.com>
2026-08-24 22:43:45 -0300

    more features

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const resetButton = document.querySelector('.reset-btn');
 const flipBtn = document.querySelector('.flip-btn');
 const cursor = document.querySelector('.pointer');
 const reloadBtn = document.querySelector('.reload-btn');
+const ammoCounter = document.querySelector('.ammo');
+
+let ammo = 5;
 
 const sounds = {
   audio1: new Audio('sounds/fire-whoosh.wav'),
   audio2: new Audio('sounds/gun-reloading.mp3'),
-  audio3: new Audio('sounds/peproll1.mp3')
+  audio3: new Audio('sounds/peproll1.mp3'),
+  audio4: new Audio('sounds/wrong-answer-sound-effect.mp3')
 }
 
 
@@ -38,21 +42,15 @@ function timer() {
   }, 1000)
 }
 
-
-
-fireBtn.addEventListener('click', () => {
-  sounds.audio1.play();
-});
-
-flipBtn.addEventListener('click', () => {
-  sounds.audio3.play();
-});
-
-
-
 let shotN = JSON.parse(localStorage.getItem('shots')) ?? 0; /*This is for users that will join the site for the first time.*/
 
   fireBtn.addEventListener('click', () => {
+
+    ammoCounter.innerHTML = `AMMO: ${ammo}`;
+
+    ammo--;
+
+    sounds.audio1.play();
 
     shotN++;
 
@@ -60,7 +58,10 @@ let shotN = JSON.parse(localStorage.getItem('shots')) ?? 0; /*This is for users 
 
     shotCounter.innerHTML = `<p>You shot: ${shotN}</p>`;
 
-
+    if (ammo <= 0) {
+      sounds.audio4.play();
+      ammo = 0;
+    }
 
   });
 
@@ -74,6 +75,10 @@ let shotN = JSON.parse(localStorage.getItem('shots')) ?? 0; /*This is for users 
 
   reloadBtn.addEventListener('click', () => {
 
+    if (ammo < 5) {
+      ammo ++;
+      ammoCounter.innerHTML = `AMMO: ${ammo}`;
+
     sounds.audio2.play();
 
     cursor.classList.remove('reload');
@@ -81,10 +86,15 @@ let shotN = JSON.parse(localStorage.getItem('shots')) ?? 0; /*This is for users 
     void cursor.offsetWidth
 
     cursor.classList.add('reload');
+    } else {
+      sounds.audio4.play();
+    }
+
   })
 
   flipBtn.addEventListener('click', () => {
 
+    sounds.audio3.play();
     /*if (cursor.classList.contains('spin')) {
       cursor.classList.remove('spin');
     } else {
